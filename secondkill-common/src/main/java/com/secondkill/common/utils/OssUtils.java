5392d0ebce6443b58d9c87c25487ea4866ca360b
choyrunyu <1063978024@qq.com>
2021-07-26 10:45:43 +0800

    修改oss读取配置为properties文件

--- secondkill-common/src/main/java/com/secondkill/common/utils/OssUtils.java
+++ secondkill-common/src/main/java/com/secondkill/common/utils/OssUtils.java
@@ -6,11 +6,14 @@ import com.aliyun.oss.model.PutObjectRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 import java.io.File;
 
 @Component
+@PropertySource("classpath:oss.properties")
 public class OssUtils {
 
     private final static Logger logger = LoggerFactory.getLogger(OssUtils.class);
